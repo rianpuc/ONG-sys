99c25d2d71c2539d59b6f0fcbb308abb910db324
rianpuc <redlopes@sga.pucminas.br>
2025-07-14 15:40:05 -0300

    UPDATE: buttons style

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -8,19 +8,19 @@ interface ButtonProps {
 }
 
 const mapper = {
-    "Criar": "bg-insertbtn-100 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg",
-    "Procurar": "bg-searchbtn-100 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg",
-    "Atualizar": "bg-updatebtn-100 hover:bg-updatebtn-100/80 text-white font-bold py-2 px-4 rounded-lg",
-    "Deletar": "bg-deletebtn-100 hover:bg-deletebtn-100/80 text-white font-bold py-2 px-4 rounded-lg",
-    "Plano": "bg-stone-500 hover:bg-stone-400 text-white font-bold py-2 px-4 border-b-4 border-stone-700 hover:border-stone-500 rounded"
+    "Criar": "transition-colors bg-insertbtn-100 hover:bg-green-700 text-white cursor-pointer font-bold py-2 px-4 rounded-lg",
+    "Procurar": "transition-colors bg-searchbtn-100 hover:bg-blue-700 text-white cursor-pointer font-bold py-2 px-4 rounded-lg",
+    "Atualizar": "transition-colors bg-updatebtn-100 hover:bg-updatebtn-100/80 cursor-pointer text-white font-bold py-2 px-4 rounded-lg",
+    "Deletar": "transition-colors bg-deletebtn-100 hover:bg-deletebtn-100/80 cursor-pointer text-white font-bold py-2 px-4 rounded-lg",
+    "Plano": "transition-colors bg-stone-500 hover:bg-stone-400 text-white cursor-pointer font-bold py-2 px-4 border-b-4 border-stone-700 hover:border-stone-500 rounded"
 }
 
 const disabledMapper = {
-    "Criar": "opacity-80 bg-insertbtn-100 text-white font-bold py-2 px-4 rounded-lg",
-    "Procurar": "opacity-80 bg-searchbtn-100 text-white font-bold py-2 px-4 rounded-lg",
-    "Atualizar": "opacity-80 bg-updatebtn-100 text-white font-bold py-2 px-4 rounded-lg",
-    "Deletar": "opacity-80 bg-deletebtn-100 text-white font-bold py-2 px-4 rounded-lg",
-    "Plano": "opacity-75 bg-stone-400 text-white font-bold py-2 px-4 border-b-4 border-stone-700 hover:border-stone-500 rounded"
+    "Criar": "cursor-not-allowed opacity-80 bg-insertbtn-100 text-white font-bold py-2 px-4 rounded-lg",
+    "Procurar": "cursor-not-allowed opacity-80 bg-searchbtn-100 text-white font-bold py-2 px-4 rounded-lg",
+    "Atualizar": "cursor-not-allowed opacity-80 bg-updatebtn-100 text-white font-bold py-2 px-4 rounded-lg",
+    "Deletar": "cursor-not-allowed opacity-80 bg-deletebtn-100 text-white font-bold py-2 px-4 rounded-lg",
+    "Plano": "cursor-not-allowed opacity-75 bg-stone-400 text-white font-bold py-2 px-4 border-b-4 border-stone-700 hover:border-stone-500 rounded"
 }
 
 const Button = ({ name, children, disabled, onClick }: ButtonProps) => {
